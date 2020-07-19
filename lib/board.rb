465bdf71be290f9aa3b5eed71f1beeec64ba6acf
Marcus Quintana <mq97325@gmail.com>
2020-07-19 21:00:59 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,4 @@
-# Define the variable board below.
-row1 = ["","",""]
-row2 = ["","",""]
-row3 = ["","",""]
-row2[1] = "X"
-board = [row1,row2,row3]
+# Define the variable board below
+board = ["","",""]
+["","",""]
+["","",""]
